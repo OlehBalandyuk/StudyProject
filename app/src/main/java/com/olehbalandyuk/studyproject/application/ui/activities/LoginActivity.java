b67311225aa9807cdfdcd448232dd856ed90e4fb
Vaschenko-Volodymyr <vaschenko.volodymyr@gmail.com>
2016-06-06 18:07:43 +0300

    activity-login-xml-done

---
+++ app/src/main/java/com/olehbalandyuk/studyproject/application/ui/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.olehbalandyuk.studyproject.application.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.olehbalandyuk.studyproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
